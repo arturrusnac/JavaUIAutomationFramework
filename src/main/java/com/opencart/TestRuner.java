2832432d56437c68d201d5f64e805d248809d529
arturrrusnac <artur.rusnac@gmail.com>
2023-06-16 19:45:06 +0300

    Set up the project structure by adding the necessary components and removing the unuseful ones.

---
+++ src/main/java/com/opencart/TestRuner.java
@@ -0,0 +1,7 @@
+package com.opencart;
+
+public class TestRuner {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/com/opencart/managers/ExampleManager.java
@@ -0,0 +1,6 @@
+package com.opencart.managers;public class ExampleManager {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
